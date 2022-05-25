3b07ff9dfe05bc94bf4d19df0a3d67155867ca66
clogerie <66565287+clogerie@users.noreply.github.com>
2022-05-25 18:28:51 -0400

    added functions to save and store time block descriptions

--- script.js
+++ script.js
@@ -1,63 +1,76 @@
-/***********************************************
-   REMOVE COMMENTS BEFORE SUBMITTING YOUR CODE
-************************************************/
 $(document).ready(function() {
-  var intervalId = setInterval(function() {
+    var intervalId = setInterval(function() {
     var currentDate = moment();
     $("#currentDay").text(currentDate.format("dddd, MMMM Do YYYY, h:mm:ss a"));
     console.log(intervalId);
   }, 1000);
   
-  // listen for save button clicks
+  // function to save time block descriptions
   function saveDescription() {
-      // get nearby values;
-      var saveBtn = $(".saveBtn");
-      // Store the parent id of the save button clicked to a variable and use it as key to localStorage
-      // Hint: parent id is stored in the id attribute of the parent
-      var nineAm = $("#nine-am")
-    
-      // store user input in textarea to a variable
-      // Hint: textarea is one of siblings of the clicked button or could be traversed as one of children of the parent of save button
-      var userDescription = $(".description").val().trim();
-    
-      // Save key and value in variables in localStorage
-    
-      // Optional: Show notification that item was saved to localStorage by adding class 'show'
-      // Hint: grab the notification element and add class
-    
-      // Optional: Timeout to remove 'show' class after 5 seconds
-      // Hint: global timeout function that only get invoke once
-    
+      var hr9 = $("#9").val();
+      localStorage.setItem("desc9", hr9);
+
+      var hr10 = $("#10").val();
+      localStorage.setItem("desc10", hr10);
+
+      var hr11 = $("#11").val();
+      localStorage.setItem("desc11", hr11);
+
+      var hr12 = $("#12").val();
+      localStorage.setItem("desc12", hr12);
+
+      var hr13 = $("#13").val();
+      localStorage.setItem("desc13", hr13);
+
+      var hr14 = $("#14").val();
+      localStorage.setItem("des14", hr14);
+
+      var hr15 = $("#15").val();
+      localStorage.setItem("desc15", hr15);
+
+      var hr16 = $("#16").val();
+      localStorage.setItem("desc16", hr16);
+
+      var hr17 = $("#17").val();
+      localStorage.setItem("desc17", hr17);
     };
-    
+    //event listener for save button
     $(".saveBtn").on("click", saveDescription);
-    // Alternative way to set up jquery event listener
-    // $('.saveBtn').click(doSomethingAboutClickOrSaveButton);
+
+
+    // render time block descriptions to corresponding time block
+    var hour9Desc = localStorage.getItem("desc9");
+    $("#9").val(hour9Desc);
+
+    var hour10Desc = localStorage.getItem("desc10");
+    $("#10").val(hour10Desc);
+
+    var hour11Desc = localStorage.getItem("desc11");
+    $("#11").val(hour11Desc);
+
+    var hour12Desc = localStorage.getItem("desc12");
+    $("#12").val(hour12Desc);
+
+    var hour13Desc = localStorage.getItem("desc13");
+    $("#13").val(hour13Desc);
+
+    var hour14Desc = localStorage.getItem("desc14");
+    $("#14").val(hour14Desc);
+
+    var hour15Desc = localStorage.getItem("desc15");
+    $("#15").val(hour15Desc);
+
+    var hour16Desc = localStorage.getItem("desc16");
+    $("#16").val(hour16Desc);
+
+    var hour17Desc = localStorage.getItem("desc17");
+    $("#17").val(hour17Desc);
     
     function updateTimeBlockColor() {
-      // get current number of hours and store it to a variable
-      // Hint: get current hour from moment js
-    
-      // Loop over time blocks
-      // get each time block element and retrieve id attribute
-      // retrieve the number from id and convert it into integer
-      // store the integer to a variable
-      // compare the value of this variable with the current hour
-      // add class accordingly based on whether this time block element is in the past, or present
-      // Hint: remove previous class values before adding class
+     
     
     }
     
     updateTimeBlockColor();
-    
-    // set up interval to check if current time needs to be updated
     var interval = setInterval(updateTimeBlockColor, 1000);
 });
-
-  
-  // load any saved data from localStorage
-  // Hint: identify each textarea elements using parent id and its class
-  
-  // display current day on page using moment js
-  
-  // Optional: what does $(document).ready(function() {}) do and how can you add it to your code
