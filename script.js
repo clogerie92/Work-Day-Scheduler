62f2bd22cc30ff6ea091540a0d3c2aa24de2110f
clogerie <66565287+clogerie@users.noreply.github.com>
2022-05-26 19:52:33 -0400

    cleaning up code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+    // get current hour
+    var currentHour = moment().format("h");
+    // gets current time and day for jumbotron
     var intervalId = setInterval(function() {
     var currentDate = moment();
     $("#currentDay").text(currentDate.format("dddd, MMMM Do YYYY, h:mm:ss a"));
@@ -8,90 +10,45 @@ $(document).ready(function() {
   
   // function to save time block descriptions
   function saveDescription(event) {
-      // var hr9 = $("#9").val();
-      // localStorage.setItem("desc9", hr9);
+    event.preventDefault();
 
-      // var hr10 = $("#10").val();
-      // localStorage.setItem("desc10", hr10);
-
-      // var hr11 = $("#11").val();
-      // localStorage.setItem("desc11", hr11);
-
-      // var hr12 = $("#12").val();
-      // localStorage.setItem("desc12", hr12);
-
-      // var hr13 = $("#13").val();
-      // localStorage.setItem("desc13", hr13);
-
-      // var hr14 = $("#14").val();
-      // localStorage.setItem("des14", hr14);
-
-      // var hr15 = $("#15").val();
-      // localStorage.setItem("desc15", hr15);
-
-      // var hr16 = $("#16").val();
-      // localStorage.setItem("desc16", hr16);
-
-      // var hr17 = $("#17").val();
-      // localStorage.setItem("desc17", hr17);
-      event.preventDefault();
-
-      var timeBlockVal = $(this).siblings(".timeBlock").val();
-      var hour = $(this).parent().attr("id").split("-")[1];
-      localStorage.setItem(hour, timeBlockVal);
-      console.log(timeBlockVal);
-      console.log(hour);
-
-
+    var timeBlockVal = $(this).siblings(".timeBlock").val();
+    var hour = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(hour, timeBlockVal);
+    console.log(timeBlockVal);
+    console.log(hour);
     };
-    //event listener for save button
+    
+    // gets data from local storage and renders them to corresponding time block
+    $("#hour-9 .timeBlock").val(localStorage.getItem("9"));
+    $("#hour-10 .timeBlock").val(localStorage.getItem("10"));
+    $("#hour-11 .timeBlock").val(localStorage.getItem("11"));
+    $("#hour-12 .timeBlock").val(localStorage.getItem("12"));
+    $("#hour-13 .timeBlock").val(localStorage.getItem("13"));
+    $("#hour-14 .timeBlock").val(localStorage.getItem("14"));
+    $("#hour-15 .timeBlock").val(localStorage.getItem("15"));
+    $("#hour-16 .timeBlock").val(localStorage.getItem("16"));
+    $("#hour-17 .timeBlock").val(localStorage.getItem("17"));
+
+    // update time block color for each element with class time-block
+    $(".time-block").each(function updateTimeBlockColor() {
+      var timeRow = $(this).attr("id").split("-")[1];
+
+      if (currentHour === timeRow) {
+        $(this).addClass("present");
+      } else if (currentHour < timeRow) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else if (currentHour > timeRow) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+    });
+    
+
+    // event listener for save button
     $(".saveBtn").on("click", saveDescription);
 
-
-    // render time block descriptions to corresponding time block
-    var hour9Desc = localStorage.getItem("desc9");
-    $("#9").val(hour9Desc);
-
-    var hour10Desc = localStorage.getItem("desc10");
-    $("#10").val(hour10Desc);
-
-    var hour11Desc = localStorage.getItem("desc11");
-    $("#11").val(hour11Desc);
-
-    var hour12Desc = localStorage.getItem("desc12");
-    $("#12").val(hour12Desc);
-
-    var hour13Desc = localStorage.getItem("desc13");
-    $("#13").val(hour13Desc);
-
-    var hour14Desc = localStorage.getItem("desc14");
-    $("#14").val(hour14Desc);
-
-    var hour15Desc = localStorage.getItem("desc15");
-    $("#15").val(hour15Desc);
-
-    var hour16Desc = localStorage.getItem("desc16");
-    $("#16").val(hour16Desc);
-
-    var hour17Desc = localStorage.getItem("desc17");
-    $("#17").val(hour17Desc);
-    
     // function to update time block color
-    var currentHour = moment().hour();
-    console.log(currentHour);
-    function updateTimeBlockColor() {
-      for (var i = 0; i < hours.length; i++) {
-        if (hours < currentHour) {
-          $(hours.toString()).addClass("past");
-        } else if (hours === currentHour) {
-          $(hours.toString()).addClass("present");
-        } else if (hours > currentHour) {
-          $(hours.toString()).addClass("future");
-        }
-      }
-    
-    }
-    
     updateTimeBlockColor();
-    var interval = setInterval(updateTimeBlockColor, 1000);
 });
